7f28064626484bc156860c3992e1b8940d30e443
Rafal Ledwon <rafalledwon@gmail.com>
2018-07-03 16:24:41 +0200

    Game pongo

--- js/app.js
+++ js/app.js
@@ -3,12 +3,17 @@ let canvas,
     ballX = 50,
     ballY = 50,
     ballSpeedX = 5,
-    ballSpeedY = 2,
+    ballSpeedY = 5,
     paddle1Y = 250,
-    paddle2Y = 250;
+    paddle2Y = 250,
+    player1Score = 0,
+    player2Score = 0,
+    winScreen = false;
+
 
 const paddleHeight = 100,
-    paddleWidth = 10;
+      paddleWidth = 10,
+      winningScore = 5;
 
 calculateMousePosition=(e)=>{
     let rectangle = canvas.getBoundingClientRect();
@@ -30,30 +35,55 @@ window.onload =()=> {
 
     canvas.addEventListener('mousemove',(e)=>{
             let mousePos = calculateMousePosition(e);
-            paddle2Y = mousePos.y -(paddleHeight/2);
+            paddle1Y = mousePos.y -(paddleHeight/2);
     })
 };
 ballReset =()=>{
+    if (player1Score >= winningScore || player2Score >= winningScore){
+        player1Score = 0;
+        player2Score = 0;
+        winScreen = true;
+    }
     ballSpeedX = -ballSpeedX;
     ballX = canvas.width/2;
     ballY = canvas.height/2;
 };
 
-moveEverything=()=>{
-    ballX = ballX + ballSpeedX;
-    ballY = ballY + ballSpeedY;
+computerMovement =()=>{
+    const paddle2Ycenter = paddle2Y + (paddleHeight/2);
+    if(paddle2Ycenter < ballY -35) {
+        paddle2Y += 6;
+    }else if (paddle2Ycenter > ballY +35){
+        paddle2Y -= 6;
+    }
+};
+
+moveEverything =()=>{
+    if(winScreen){
+        return;
+    }
+    computerMovement();
+
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
 
     if (ballX < 0) {
         if(ballY > paddle1Y && ballY <paddle1Y +paddleHeight){
-            ballSpeedX = -ballSpeedX
+            ballSpeedX = -ballSpeedX;
+            let deltaY = ballY -(paddle1Y +paddleHeight/2);
+                ballSpeedY = deltaY * 0.35;
         }else {
+            player2Score += 1;
             ballReset();
         }
     }
     if (ballX > canvas.width -10) {
         if(ballY > paddle2Y && ballY <paddle2Y +paddleHeight){
-            ballSpeedX = -ballSpeedX
+            ballSpeedX = -ballSpeedX;
+            let deltaY = ballY -(paddle2Y +paddleHeight/2);
+            ballSpeedY = deltaY * 0.35;
         }else {
+            player1Score +=1;
             ballReset();
         }
     }
@@ -66,6 +96,10 @@ moveEverything=()=>{
 };
 
 drawEverything=()=> {
+    if(winScreen){
+        canvasContext.fillText('click to continue', 50, 50);
+        return;
+    }
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
     //ball size
@@ -77,6 +111,8 @@ drawEverything=()=> {
     //paddle right
     canvasContext.fillStyle = 'white';
     canvasContext.fillRect(990,paddle2Y, paddleWidth, paddleHeight);
-
+    //Score
+    canvasContext.fillText(player1Score, 50, 50);
+    canvasContext.fillText(player2Score, 950, 50);
 
 };
